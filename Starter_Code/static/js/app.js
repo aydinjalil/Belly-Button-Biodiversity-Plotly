5f471a1b60890d67dce8a03d35a6e6d5e35fdaad
aydin <aydinjalil@gmail.com>
2020-03-29 22:02:41 -0400

    minor changes to the code

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -12,39 +12,33 @@ function init(){
 			.text(sample)
 			.property("value", sample);
 		});
-		plot_bar(sampleNames.names[0]);
+		chart(sampleNames.names[0]);
 	});
 }
 
 
 
-function plot_bar(id){
+function chart(id){
 	d3.json("../../samples.json").then((sample_collection) => {
 		sample_collection.samples.forEach((otu)=>{
 			if (otu.id === id){
-				var trace = {
-		    	x: otu.otu_ids.slice(0,10),
-		    	y: otu.sample_values.slice(0,10),
-		    	// mode: "markers",
-		    	text: otu.otu_labels,
-		    	marker: {
-		    		color: otu.otu_ids,
-		    		size: otu.sample_values
-		    	},
-		   	 	type: "bar"
-  			};
-
-  			var data = [trace];
+				var data = [{
+			    	x: otu.otu_ids.slice(0,10),
+			    	y: otu.sample_values.slice(0,10),
+			    	mode: "markers",
+			    	text: otu.otu_labels,
+			    	marker: {
+			    		color: otu.otu_ids,
+			    		size: otu.sample_values,
+			    		width: 1
+			    	},
+			   	 	type: "bar",
+			   	 	orientation: "h"
+  				}];
+  				Plotly.newPlot("bar", data);
 			}
 			
-
-  			// var layout = {
-			    
-			  //   margin:{t: 20, r: 20, b: 30, l: 40},
-	  		// };
-
-	  		Plotly.newPlot("bar", data);
-		})
+		});
 	});
 	
 	
@@ -54,11 +48,12 @@ function plot_bar(id){
 
   // Plot the chart to a div tag with id "bar-plot"
 }
-// function optionChanged() {
+function optionChanged() {
 
-// 	selectValue = d3.select('select').property('value')
-// 	d3.select('body')
-// 		.append('p')
-// 		.text(selectValue + ' is the last selected option.')
-// };
+	selectValue = d3.select('select').property('value')
+	d3.select('body')
+		.append('p')
+		.text(selectValue + ' is the last selected option.')
+};
 init();
+
